f1743965ba32a32c9b59a380376ceaff8f4804c0
vmerkle8331 <vmerkle@uccs.edu>
2023-04-29 01:01:47 +0000

    fucking finally

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :books
+  devise_for :users
+  #resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "books#index"
   resources :books
+
+  #root "home#index"
 end
