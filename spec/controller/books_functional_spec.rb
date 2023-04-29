28600e9d65a5c12bfdaaec2c07376df1b607a016
vmerkle8331 <vmerkle@uccs.edu>
2023-04-29 01:23:35 +0000

    fucking finally

--- spec/controller/books_functional_spec.rb
+++ spec/controller/books_functional_spec.rb
@@ -1,5 +1,12 @@
 require "rails_helper"
 RSpec.describe BooksController, :type => :controller do
+    login_user
+    let(:valid_attributes) {
+        { :title => "Test title!", :world => "Test world!", :description => "This is a test description", :quote => "Test quote" }
+    }
+
+    let(:valid_session) { {} }
+    
     describe "test web requests" do
         context "GET #index" do
             it "returns a success response" do
